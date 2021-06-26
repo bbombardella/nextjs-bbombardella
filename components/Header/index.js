695468afe67406812b1804653afc5e152811685a
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2021-06-26 16:55:33 +0200

    Layout + Header fix

--- components/Header/index.js
+++ components/Header/index.js
@@ -6,16 +6,22 @@ import styles from '../../styles/Header.module.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function Header() {
-    console.log(styles);
+    let isOpen = false
     return (
         <Row className={styles['header-container']}>
             <Col>
                 <header className={styles.header}>
                     <div className={styles.logo}>
-                        <Image src={logo} alt="cc" />
+                        <div className={styles['logo-img']}>
+                            <Image src={logo} alt="Logo" layout="responsive" />
+                        </div>
                     </div>
                     <h1 className={styles.title}>BASTIEN BOMBARDELLA</h1>
-                    <p className={styles.menu}>menu</p>
+                    <div className={styles.menu}>
+                        <svg className={styles['menu-img']+" bi bi-list"} onClick={() => isOpen = !isOpen} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                        </svg>
+                    </div>
                 </header>
             </Col>
         </Row>
