53073f12c3bccc8453e0ba3620fbfc5804b26f76
BASTIEN BOMBARDELLA <bastien.bombardella@etu.univ-lyon1.fr>
2021-06-27 11:07:52 +0200

    Typographie

--- components/HomeParticles/index.js
+++ components/HomeParticles/index.js
@@ -1,17 +1,29 @@
 import React, { useEffect } from "react"
+import Typewriter from 'typewriter-effect'
 import { tsParticles } from "tsparticles"
 import settings from './settings.json'
+import strings from './strings.json'
 import styles from '../../styles/Particles.module.scss'
 
 export default function HomeParticles() {
     useEffect(() => {
         const pathseg = require("pathseg")
         tsParticles.load("tsparticles", settings)
+        console.log(strings)
     })
 
     return (
         <div className={styles.wrapper}>
-            <p className={styles.text}>Je suis ...</p>
+            <span className={styles.text}>Je suis<span> </span>
+                <Typewriter className={styles.text}
+                    options={{
+                        strings: strings,
+                        autoStart: true,
+                        loop: true,
+                        cursor: "_"
+                    }}
+                />
+            </span>
             <div className={styles.particles} id="tsparticles">
             </div>
         </div>
